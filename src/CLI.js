40c2dc42f62fe03f817592cce0c9dff3b8319b78
Karl Thompson <kthompson@student.neumont.edu>
2015-04-14 14:54:45 -0600

    Able to read from the package.json file and can bump the version, but the results are not saved back to the package.json

--- src/CLI.js
+++ src/CLI.js
@@ -2,14 +2,19 @@ import VersionBumper from "./version-bump.js";
 import Yargs from "Yargs";
 import Package from "../package.json";
 
+let bumper = new VersionBumper();
+
 let yarguments = Yargs
-	.usage('Usage: release <version-type> --preid <id>')
-	.help('Version Types are:\nmajor\tminor\tpatch\n\t\tpremajor\tpreminor\tprepatch\n\nPre-ids shoudld be one word. Any pre-id that is multiple words will be concatentated into one (e.g. "this that" becomes "thisthat"')
+	.usage('Usage: bin/release <version-type> --preid <id>')
+	.help('Version Types are:\nmajor\tminor\tpatch\n\t\tpremajor\tpreminor\tprepatch\n\nPre-ids shoudld be one word. Any pre-id that is multiple words will be concatentated into one (e.g. "this that" becomes "thisthat"\n')
 	.alias('h','help')
-	.command('version','Get version number')
+	.command('version','Get current version')
 	.alias('v','version')
+	.command('bump','Bump version, followed by update type')
+	.alias('b','bump')
 	.command('preid','Set prerelease tag')
-	.alias('p','preid');
+	.alias('p','preid')
+	.implies('preid','updateType');
 
 if(yarguments.argv.help)
 {
@@ -21,11 +26,19 @@ else if(yarguments.argv.version)
 }
 else
 {
+	console.log("Previous version: " + Package.version);
+	let preid;
 	if(yarguments.argv.preid)
 	{
-		let preid = '';
 		let args  = yarguments.argv.preid.split(' ');
 		args.forEach(function(word){preid += word.toLowerCase();});
 		console.log(preid);
+	}
+	let updateType = yarguments.argv.bump;
+	try{
+		let updateVersion = bumper.versionBump(Package.version,updateType,preid);
+		console.log("Current version: " + updateVersion);
+	}catch(err){
+		console.log("The following error occurred: " + err);
 	}
 }
